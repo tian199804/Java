1a3f5ef6de65addb6f6df5c4e5628b93b7be22f2
tiantiantian <2399318317@qq.com>
2020-05-06 20:54:43 +0800

    update

---
+++ Shape/src/club/banyuan/area/Rectangle.java
@@ -0,0 +1,22 @@
+package club.banyuan.area;
+
+public class Rectangle extends Shape {
+    private int a;
+    private int b;
+    @Override
+    public String getShapeName() {
+        return "矩形";
+    }
+    public Rectangle(int a,int b) {
+        this.a=a;
+        this.b=b;
+    }
+    @Override
+    public double area() {
+        return a*b;
+    }
+    @Override
+    public double perimeter() {
+        return 2 * (a+b);
+    }
+}
